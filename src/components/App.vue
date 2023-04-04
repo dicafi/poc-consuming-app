8e2454e22660717baf094fb3ca326aa5816f28fe
Julio Acevedo <dicafis@gmail.com>
2023-04-03 21:21:46 -0600

    Add remote component consumed from a different app

---
+++ src/components/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div>
+    <router-view />
+
+    <exposed origin="remote"/>
+
+	<br />
+	 A demo of a single page application made in Vue.js, PostCSS and Bootstrap
+
+  </div>
+</template>
+
+<script>
+  import exposed from 'exposing/Exposed'
+
+  export default {
+    components: {
+      exposed,
+    }
+  }
+</script>
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import AppHome from './components/AppHome.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: AppHome
+    },
+];
+
+export default routes;
